4ce41ff18b5c44cf5e51cf6ae3ba015b769d22a9
theangelskies <theangelskies13@yahoo.com>
2026-02-28 01:40:06 +0100

    getElementById for user and content

--- script.mjs
+++ script.mjs
@@ -1,2 +1,5 @@
 import { getUserIDs } from "./data.mjs";
 import { analyzeUser, formatSong } from "./common.mjs";
+
+const userSelect = document.getElementById("user-select");
+const content = document.getElementById("content");
