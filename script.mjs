369896439f39f61dbf07eb66a6fbdd18def940ad
theangelskies <theangelskies13@yahoo.com>
2026-02-28 01:40:42 +0100

    populate dropdown

--- script.mjs
+++ script.mjs
@@ -3,3 +3,10 @@ import { analyzeUser, formatSong } from "./common.mjs";
 
 const userSelect = document.getElementById("user-select");
 const content = document.getElementById("content");
+// Populate dropdown
+getUserIDs().forEach((id) => {
+  const option = document.createElement("option");
+  option.value = id;
+  option.textContent = `User ${id}`;
+  userSelect.appendChild(option);
+});
