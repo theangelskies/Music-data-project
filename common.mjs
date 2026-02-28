4252a90c9e91a6b08713e9bbb943551cdc659b43
theangelskies <theangelskies13@yahoo.com>
2026-02-28 01:09:58 +0100

    Add additional imports and structure for basic helper functions

--- common.mjs
+++ common.mjs
@@ -1,3 +1,12 @@
-import { getUserIDs } from "./data.mjs";
+import { getUserIDs, getListenEvents, getSong } from "./data.mjs";
+
+// ----------------------
+// Basic Helpers
+// ----------------------
 
 export const countUsers = () => getUserIDs().length;
+
+export const countBy = (arr, keyFn) => {
+
+  return map;
+};
