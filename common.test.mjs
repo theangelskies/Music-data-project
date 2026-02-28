298954d0f0e42d60a3f6e59ae369d2d039b4def3
theangelskies <theangelskies13@yahoo.com>
2026-02-28 01:31:12 +0100

    all import

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,12 @@
-import assert from "node:assert";
 import test from "node:test";
-import { countUsers } from "./common.mjs";
-
-test("User count is correct", () => {
-  assert.equal(countUsers(), 4);
-});
+import {
+  countUsers,
+  countBy,
+  maxByValue,
+  formatSong,
+  filterFridayNight,
+  getLongestStreak,
+  getEveryDaySongs,
+  getTopGenres,
+  analyzeUser,
+} from "./common.mjs";
